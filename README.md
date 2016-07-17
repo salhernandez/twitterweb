e058053af8860ec38c0a1e3b29e178b6d2758b8b
salhernandez <hernandezg.sal@gmail.com>
2016-07-16 20:14:54 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#twitterweb
+
+Used to quickly find out what is going on at twitter by streamlining the search feature as well as the user search so that all of the information can be comfortably accessed, stored, and viewed from the local machine.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='twitterweb',
+      version='0.0.1',
+      description='Web wrapper for twitter',
+      url='https://github.com/salhernandez/letmeknow',
+      author='Salvador Hernandez',
+      author_email='hernandezg.sal@gmail.com',
+      license='MIT',
+      packages=['twitterweb'],
+      zip_safe=False)
